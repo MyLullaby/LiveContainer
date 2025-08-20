d4a67a1557d96b9cecb6fddf4e0b3d8182bbabbf
Lullaby <lullaby0906@outlook.com>
2025-08-20 18:02:36 +0800

    尝试增加第三个LiveContainer 修复图标

--- LiveContainerSwiftUI/LCUtils.m
+++ LiveContainerSwiftUI/LCUtils.m
@@ -478,17 +478,17 @@ Class LCSharedUtilsClass = nil;
         [infoDict[@"CFBundleURLTypes"] removeLastObject];
     }
     [infoDict removeObjectForKey:@"UTExportedTypeDeclarations"];
-    infoDict[@"CFBundleIconName"] = @"AppIconGrey";
+    infoDict[@"CFBundleIconName"] = @"ThirdIcon";
     if (infoDict[@"CFBundleIcons"][@"CFBundlePrimaryIcon"][@"CFBundleIconName"]) {
         infoDict[@"CFBundleIcons"][@"CFBundlePrimaryIcon"][@"CFBundleIconName"] = @"ThirdIcon";
     }
-    infoDict[@"CFBundleIcons"][@"CFBundlePrimaryIcon"][@"CFBundleIconFiles"][0] = @"third_icon60x60";
+    infoDict[@"CFBundleIcons"][@"CFBundlePrimaryIcon"][@"CFBundleIconFiles"][0] = @"ThirdIcon60x60";
     
     if (infoDict[@"CFBundleIcons~ipad"][@"CFBundlePrimaryIcon"][@"CFBundleIconName"]) {
         infoDict[@"CFBundleIcons~ipad"][@"CFBundlePrimaryIcon"][@"CFBundleIconName"] = @"ThirdIcon";
     }
-    infoDict[@"CFBundleIcons~ipad"][@"CFBundlePrimaryIcon"][@"CFBundleIconFiles"][0] = @"third_icon60x60";
-    infoDict[@"CFBundleIcons~ipad"][@"CFBundlePrimaryIcon"][@"CFBundleIconFiles"][1] = @"third_icon76x76";
+    infoDict[@"CFBundleIcons~ipad"][@"CFBundlePrimaryIcon"][@"CFBundleIconFiles"][0] = @"ThirdIcon60x60";
+    infoDict[@"CFBundleIcons~ipad"][@"CFBundlePrimaryIcon"][@"CFBundleIconFiles"][1] = @"ThirdIcon76x76";
     
     // reset a executable name so they don't look the same on the log
     NSURL* appBundlePath = [tmpPayloadPath URLByAppendingPathComponent:@"App.app"];
