ee93596ffccea373fb2947fb275090d48988ae67
blandli <blandli@tencent.com>
2026-09-13 23:34:47 +0800

    Restore maintained LiveContainer SideStore nightly dependency

--- .github/build_github.sh
+++ .github/build_github.sh
@@ -41,7 +41,7 @@ mv ./tmp/SideStoreSupport.framework Payload/LiveContainer.app/Frameworks
 
 # download SideStore
 cd tmp
-wget https://github.com/MyLullaby/SideStore/releases/download/nightly/SideStore.ipa
+wget https://github.com/LiveContainer/SideStore/releases/download/nightly/SideStore.ipa
 unzip SideStore.ipa
 cd ..
 
